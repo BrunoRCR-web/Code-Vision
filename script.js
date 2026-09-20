51d3ea05f2f9f626f771e3b84eeb67793b469e50
CodeVision <brunorotundocamargo@gmail.com>
2026-09-20 00:37:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -61,3 +61,36 @@ elementosAnimados.forEach(function (elemento) {
     );
 
 });
+
+// MENU MOBILE
+
+const menuBotao = document.querySelector(".menu-mobile");
+const menuNav = document.querySelector("header nav");
+const linksMenu = document.querySelectorAll("header nav a");
+
+menuBotao.addEventListener("click", () => {
+
+    menuNav.classList.toggle("menu-aberto");
+
+    if (menuNav.classList.contains("menu-aberto")) {
+        menuBotao.innerHTML = "<span>×</span>";
+        menuBotao.setAttribute("aria-label", "Fechar menu");
+    } else {
+        menuBotao.innerHTML = "<span>☰</span>";
+        menuBotao.setAttribute("aria-label", "Abrir menu");
+    }
+
+});
+
+linksMenu.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        menuNav.classList.remove("menu-aberto");
+
+        menuBotao.innerHTML = "<span>☰</span>";
+        menuBotao.setAttribute("aria-label", "Abrir menu");
+
+    });
+
+});
